8db74b8dace82fdf74ecec1b94bad9242022c7a3
The Joel <jotelfootball@gmail.com>
2026-08-29 22:35:54 +0100

    fix: remove test property from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -33,9 +33,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["./src/setupTests.ts"],
-  },
 });
